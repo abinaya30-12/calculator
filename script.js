b7f35a229dd339b8f4708407103b46cd7e30d594
abinaya30-12 <abinaya.ug.23.it@francisxavier.ac.in>
2025-08-12 14:18:22 +0530

    Added colorful calculator with memory & theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let display = document.getElementById('display');
 let historyList = document.getElementById('historyList');
 let memory = 0;
@@ -45,3 +46,52 @@ function toggleTheme() {
   document.body.classList.toggle('dark');
   document.body.classList.toggle('light');
 }
+=======
+let display = document.getElementById('display');
+let historyList = document.getElementById('historyList');
+let memory = 0;
+
+function appendValue(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let result = eval(display.value);
+    historyList.innerHTML += `<li>${display.value} = ${result}</li>`;
+    display.value = result;
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function useMemory(action) {
+  switch (action) {
+    case 'M+':
+      memory += parseFloat(display.value) || 0;
+      break;
+    case 'M-':
+      memory -= parseFloat(display.value) || 0;
+      break;
+    case 'MR':
+      display.value += memory;
+      break;
+    case 'MC':
+      memory = 0;
+      break;
+  }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+  document.body.classList.toggle('light');
+}
+>>>>>>> d186f12 (Initial commit)
